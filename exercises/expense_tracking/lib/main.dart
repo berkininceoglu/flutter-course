889b480079db144ab9129f8262a9157c4e5ff366
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-04-27 01:54:13 +0300

    expense tracking app

---
+++ exercises/expense_tracking/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:expense_tracking/transaction.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: "1",
+      title: "Groceries",
+      amount: 10.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "2",
+      title: "Meat",
+      amount: 4.99,
+      date: DateTime.now(),
+    )
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Berkin"),
+        ),
+        body: Column(
+            children: transactions.map((e) {
+          return Card(
+              child: Row(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                child: Text(
+                  e.amount.toString() + "\$",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Colors.purple,
+                  ),
+                ),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple, width: 2)),
+                padding: EdgeInsets.all(8),
+              ),
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Text(
+                  e.title,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  e.date.toString(),
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ])
+            ],
+          ));
+        }).toList()));
+  }
+}
