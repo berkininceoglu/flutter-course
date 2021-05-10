2b4b8797bef197a15b323d12a9ac6103aab1af14
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-05-10 16:19:58 +0300

    singleton mediaquery object

--- exercises/expense_tracking/lib/main.dart
+++ exercises/expense_tracking/lib/main.dart
@@ -87,8 +87,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final isLandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
     final appBar = AppBar(
       title:
           Text("Expense Tracking", style: TextStyle(fontFamily: 'Quicksand')),
@@ -101,9 +101,9 @@ class _MyHomePageState extends State<MyHomePage> {
     );
 
     final trListWidget = Container(
-        height: (MediaQuery.of(context).size.height -
+        height: (mediaQuery.size.height -
                 appBar.preferredSize.height -
-                MediaQuery.of(context).padding.top) *
+                mediaQuery.padding.top) *
             0.7,
         child: TransactionList(_userTransactions, _deleteTransaction));
     return Scaffold(
@@ -131,9 +131,9 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             if (!isLandscape)
               Container(
-                height: (MediaQuery.of(context).size.height -
+                height: (mediaQuery.size.height -
                         appBar.preferredSize.height -
-                        MediaQuery.of(context).padding.top) *
+                        mediaQuery.padding.top) *
                     0.3,
                 width: double.infinity,
                 child: Chart(_recentTransactions),
@@ -142,9 +142,9 @@ class _MyHomePageState extends State<MyHomePage> {
             if (isLandscape)
               _showChart
                   ? Container(
-                      height: (MediaQuery.of(context).size.height -
+                      height: (mediaQuery.size.height -
                               appBar.preferredSize.height -
-                              MediaQuery.of(context).padding.top) *
+                              mediaQuery.padding.top) *
                           0.7,
                       child: Chart(_recentTransactions),
                     )
